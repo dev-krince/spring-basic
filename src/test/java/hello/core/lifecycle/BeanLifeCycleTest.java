0fbada742eb0f1519956ceeb725c573ae0062f74
kimdonghyun <ssk0080@gmail.com>
2024-08-21 05:43:56 +0900

    빈 스코프 스프링은 다음과 같은 다양한 스코프를 지원한다. 싱글톤: 기본 스코프, 스프링 컨테이너의 시작과 종료까지 유지되는 가장 넓은 범위의 스코프이다. 프로토타입: 스프링 컨테이너는 프로토타입 빈의 생성과 의존관계 주입까지만 관여하고 더는 관리하지 않는 매우 짧은 범위의 스코프이다. 웹 관련 스코프 request: 웹 요청이 들어오고 나갈때 까지 유지되는 스코프이다. session: 웹 세션이 생성되고 종료될 때 까지 유지되는 스코프이다. application: 웹의 서블릿 컨텍스트와 같은 범위로 유지되는 스코프이다.

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -14,7 +14,6 @@ class BeanLifeCycleTest {
     @DisplayName("빈 생명주기 테스트")
     public void lifeCycleTest() {
         ConfigurableApplicationContext applicationContext = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
-//        NetworkClient client = applicationContext.getBean(NetworkClient.class);
         applicationContext.close();
     }
 
