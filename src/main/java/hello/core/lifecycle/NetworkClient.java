eeddd56824c8740f26369254c507c6729e419493
kimdonghyun <ssk0080@gmail.com>
2024-08-21 05:31:02 +0900

    인터페이스 InitializingBean, DisposableBean 이전 커밋에서의 결과와 다르게 테스트 코드를 동작해보면 url 이 설정된 것을 알 수 있다.

--- src/main/java/hello/core/lifecycle/NetworkClient.java
+++ src/main/java/hello/core/lifecycle/NetworkClient.java
@@ -1,16 +1,16 @@
 package hello.core.lifecycle;
 
 import lombok.Setter;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
 @Setter
-public class NetworkClient {
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void connect() {
@@ -23,5 +23,16 @@ public class NetworkClient {
 
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
     }
 }
