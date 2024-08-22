5abfbe9cec556efd6441f5731d0d7388bfde05a5
kimdonghyun <ssk0080@gmail.com>
2024-08-23 00:40:13 +0900

    프록시 객체 사용 ObjectProvider 를 사용하지 않고 프록시 객체를 사용하면 ObjectProvider 를 사용하는 코드 몇줄을 줄일 수 있다. 미리 가짜 프록시 객체를 만들어두고 빈에 등록한다. 사용하는 시점에 진짜 객체를 만들어 처리한다

--- src/main/java/hello/core/web/controller/LogDemoController.java
+++ src/main/java/hello/core/web/controller/LogDemoController.java
@@ -4,7 +4,6 @@ import hello.core.common.MyLogger;
 import hello.core.web.service.LogDemoService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,14 +13,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
 
         String requestURL = request.getRequestURI();
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
         logDemoService.logic("testId");
